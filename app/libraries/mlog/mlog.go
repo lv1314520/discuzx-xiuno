2979976ce205df67ca44d7b081fce48cd15fddfd
skiy <dev@skiy.net>
2019-06-23 18:15:15 +0800

    日志配置

--- app/libraries/mlog/mlog.go
+++ app/libraries/mlog/mlog.go
@@ -18,8 +18,8 @@ var (
 		"info":          glog.LEVEL_INFO,
 		"informational": glog.LEVEL_INFO,
 		"notice":        glog.LEVEL_NOTI | glog.LEVEL_WARN | glog.LEVEL_ERRO,
-		"warn":          glog.LEVEL_WARN | glog.LEVEL_ERRO,
-		"warning":       glog.LEVEL_WARN | glog.LEVEL_ERRO,
+		"warn":          glog.LEVEL_WARN,
+		"warning":       glog.LEVEL_WARN,
 		"error":         glog.LEVEL_ERRO,
 		"critical":      glog.LEVEL_CRIT,
 	}
@@ -34,16 +34,23 @@ func ReadLog() *Mlogger {
 	cfg := mcfg.GetCfg()
 
 	//日志等级
-	if logLevel := cfg.GetString("setting.log_level"); logLevel != "" {
+	if logLevel := cfg.GetString("log.level"); logLevel != "" {
 		if l, ok := level[logLevel]; ok {
 			Log.SetLevel(l)
 		}
 	}
 
 	//日志路径
-	if logPath := cfg.GetString("setting.log_path"); logPath != "" {
-		Log.Logger.Path(logPath)
+	if logPath := cfg.GetString("log.path"); logPath != "" {
+		if err := Log.Logger.SetPath(logPath); err != nil {
+			Log.Logger.Warning(err.Error())
+		}
 	}
+
+	// 是否输出错误行
+	Log.Logger.SetBacktrace(cfg.GetBool("log.trace"))
+
+	//Log.Logger.SetStdoutPrint(false)
 
 	return Log
 }

3bb2776692db476b2edd735e33f4aa9a42d8d1c3
skiy <dev@skiy.net>
2019-06-30 00:45:15 +0800

    update user not same uc

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -12,10 +12,32 @@ import (
 	"github.com/gogf/gf/g/util/gconv"
 )
 
-type user struct {
+/*
+User User
+ */
+type User struct {
 }
 
-func (t *user) ToConvert() (err error) {
+/*
+ToConvert user ToConvert
+ */
+func (t *User) ToConvert() (err error) {
+	cfg := mcfg.GetCfg()
+
+	if cfg.GetString("database.discuz.0.host") == cfg.GetString("database.uc.0.host") &&
+		cfg.GetString("database.discuz.0.port") == cfg.GetString("database.uc.0.port") &&
+		cfg.GetString("database.discuz.0.name") == cfg.GetString("database.uc.0.name") {
+		mlog.Log.Debug("", "%v", "same")
+
+		return t.sameUCenter()
+	}
+
+	mlog.Log.Debug("", "%v", "other")
+
+	return t.otherUCenter()
+}
+
+func (t *User) sameUCenter() (err error) {
 	start := time.Now()
 
 	cfg := mcfg.GetCfg()
@@ -28,7 +50,7 @@ func (t *user) ToConvert() (err error) {
 
 	fields := "m.uid,m.groupid,m.email,m.username,m.credits,m.regdate,s.regip,s.lastip,s.lastvisit,u.password,u.salt"
 	var r gdb.Result
-	r, err = database.GetDiscuzDB().Table(dxMemberTable+" m").LeftJoin(dxMemberStatusTable+" s", "s.uid = m.uid").LeftJoin(ucMemberTable+" u", "u.uid = m.uid").Fields(fields).Select()
+	r, err = database.GetDiscuzDB().Table(dxMemberTable + " m").LeftJoin(dxMemberStatusTable+" s", "s.uid = m.uid").LeftJoin(ucMemberTable+" u", "u.uid = m.uid").Fields(fields).Select()
 
 	xiunoTable := xiunoPre + cfg.GetString("tables.xiuno.user.name")
 	if err != nil {
@@ -43,6 +65,12 @@ func (t *user) ToConvert() (err error) {
 	xiunoDB := database.GetXiunoDB()
 	if _, err = xiunoDB.Exec("TRUNCATE " + xiunoTable); err != nil {
 		return fmt.Errorf("清空数据表(%s)失败, %s", xiunoTable, err.Error())
+	}
+
+	if cfg.GetBool("tables.xiuno.user.drop_index_email") {
+		if _, err = xiunoDB.Exec("ALTER TABLE " + xiunoTable + " DROP INDEX email"); err != nil {
+			return fmt.Errorf("表 %s 清除 email 唯一索引失败, %s", xiunoTable, err.Error())
+		}
 	}
 
 	var count int64
@@ -78,28 +106,118 @@ func (t *user) ToConvert() (err error) {
 		if batch > 1 {
 			dataList = append(dataList, d)
 		} else {
-			if res, err := xiunoDB.Insert(xiunoTable, d); err != nil {
+			res, err := xiunoDB.Insert(xiunoTable, d)
+			if err != nil {
 				return fmt.Errorf("表 %s 数据导入失败, %s", xiunoTable, err.Error())
-			} else {
-				c, _ := res.RowsAffected()
-				count += c
 			}
+
+			c, _ := res.RowsAffected()
+			count += c
 		}
 	}
 
 	if len(dataList) > 0 {
-		if res, err := xiunoDB.BatchInsert(xiunoTable, dataList, batch); err != nil {
+		res, err := xiunoDB.BatchInsert(xiunoTable, dataList, batch)
+		if err != nil {
 			return fmt.Errorf("表 %s 数据插入失败, %s", xiunoTable, err.Error())
-		} else {
-			count, _ = res.RowsAffected()
 		}
+
+		count, _ = res.RowsAffected()
 	}
 
 	mlog.Log.Info("", fmt.Sprintf("表 %s 数据导入成功, 本次导入: %d 条数据, 耗时: %v", xiunoTable, count, time.Since(start)))
 	return
 }
 
-func NewUser() *user {
-	t := &user{}
+func (t *User) otherUCenter() (err error) {
+	start := time.Now()
+
+	cfg := mcfg.GetCfg()
+
+	ucPre, discuzPre, xiunoPre := database.GetPrefix("uc"), database.GetPrefix("discuz"), database.GetPrefix("xiuno")
+
+	ucMemberTable := ucPre + "members"
+	dxMemberTable := discuzPre + "common_member"
+	dxMemberStatusTable := discuzPre + "common_member_status"
+
+	fields := "m.uid,m.groupid,m.email,m.username,m.credits,m.regdate,s.regip,s.lastip,s.lastvisit"
+	var r gdb.Result
+	r, err = database.GetDiscuzDB().Table(dxMemberTable + " m").LeftJoin(dxMemberStatusTable+" s", "s.uid = m.uid").Fields(fields).Select()
+
+	xiunoTable := xiunoPre + cfg.GetString("tables.xiuno.user.name")
+	if err != nil {
+		mlog.Log.Debug("", "表 %s 数据查询失败, %s", xiunoTable, err.Error())
+	}
+
+	if len(r) == 0 {
+		mlog.Log.Debug("", "表 %s 无数据可以转换", xiunoTable)
+		return nil
+	}
+
+	xiunoDB := database.GetXiunoDB()
+	if _, err = xiunoDB.Exec("TRUNCATE " + xiunoTable); err != nil {
+		return fmt.Errorf("表 %s 清空失败, %s", xiunoTable, err.Error())
+	}
+
+	if cfg.GetBool("tables.xiuno.user.drop_index_email") {
+		if _, err = xiunoDB.Exec("ALTER TABLE " + xiunoTable + " DROP INDEX email"); err != nil {
+			return fmt.Errorf("表 %s 清除 email 唯一索引失败, %s", xiunoTable, err.Error())
+		}
+	}
+
+	var count int64
+	fields2 := "password,salt"
+
+	for _, u := range r.ToList() {
+		password := "mustResetPassword" // 默认密码
+		salt := ""                      // 盐值
+
+		// 查询密码
+		w2 := gdb.Map{
+			"uid": u["uid"],
+		}
+		r2, err := database.GetUcDB().Table(ucMemberTable).Where(w2).Fields(fields2).One()
+		// 有错误, 或者无数据
+		if err == nil || r2 != nil {
+			password = gconv.String(r2["password"])
+			salt = gconv.String(r2["salt"])
+		}
+
+		if salt == "" {
+			salt = "20190625"
+		}
+
+		d := gdb.Map{
+			"uid":         u["uid"],
+			"gid":         u["groupid"],
+			"email":       u["email"],
+			"username":    u["username"],
+			"password":    password,
+			"salt":        salt,
+			"credits":     u["credits"],
+			"create_ip":   common.Ip2long(gconv.String(u["regip"])),
+			"create_date": gconv.Int(u["create_date"]),
+			"login_ip":    common.Ip2long(gconv.String(u["lastip"])),
+			"login_date":  gconv.Int(u["lastvisit"]),
+		}
+
+		res, err := xiunoDB.Insert(xiunoTable, d)
+		if err != nil {
+			return fmt.Errorf("表 %s 数据导入失败, %s", xiunoTable, err.Error())
+		}
+
+		c, _ := res.RowsAffected()
+		count += c
+	}
+
+	mlog.Log.Info("", fmt.Sprintf("表 %s 数据导入成功, 本次导入: %d 条数据, 耗时: %v", xiunoTable, count, time.Since(start)))
+	return
+}
+
+/*
+NewUser User init
+ */
+func NewUser() *User {
+	t := &User{}
 	return t
 }
